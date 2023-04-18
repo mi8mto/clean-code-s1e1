0be5184e80897a354f6893a262898703e1567909
mi8mto <95370082+mi8mto@users.noreply.github.com>
2023-04-19 00:13:05 +0300

    feat: add new classnames for new elements

--- app.js
+++ app.js
@@ -32,19 +32,24 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
+    listItem.className = 'list-item';
+
     label.innerText=taskString;
-    label.className='task';
+    label.className='list-item__label task';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className = "list-item__checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="list-item__input-task input-task task";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="list-item__edit edit";
 
-    deleteButton.className="delete";
+    deleteButton.className="list-item__delete delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className = 'list-item__img';
+    deleteButtonImg.setAttribute('alt', "");
     deleteButton.appendChild(deleteButtonImg);
 
 
